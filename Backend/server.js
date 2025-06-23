6b5779365bc6ef96c252550a3d4432b0889baf88
Yuvraj <yuvi97437@gamil.com>
2025-06-24 02:29:09 +0530

    m

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ await connectCloudinary();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://shivmart.onrender.com/",
+    origin: "https://shivmart.onrender.com",
     credentials: true  // ✅ this is correct
 }));
 
